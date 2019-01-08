450e0c78119cb2875cf8bdf81588aeec3a55c81e
evgeniy <evgeniy.s@live.ru>
2019-01-08 12:22:13 +0300

    Trying to resolve an issue wit CP in Windows - Russian text is not readable.

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <windows.h>
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+
     std::cout << "Здравствуй, Мир!";
     return 0;
 }
