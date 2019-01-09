0657f14a842be1206d71a50c44579598dd5dad73
EvgeniyMS <evgeniy.s@live.ru>
2019-01-09 15:31:04 +0300

    String input from console added

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <windows.h>
+using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "rus");
     SetConsoleOutputCP(1251);
-
-    std::cout << "Здравствуй, Мир!";
+    SetConsoleCP(1251);
+    
+    // std::cout << "Здравствуй, Мир!\n";
+    char string[20];
+    cin >> string; // вводим строку, используя Кириллицу
+    cout << "\nвывод: "<< string << endl; // ввывод строки
+    system("pause");
     return 0;
 }
